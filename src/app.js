c7f1d209b3628bd0e29cbfb80237f1e8ffec487a
threegeese <19970913qjh@gmail.com>
2019-09-11 16:37:15 +0800

    parsel bundle and button component

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import Button from './button.vue'
+
+new Vue({
+  el: '#app',
+  components: {
+    'g-button': Button
+  }
+})
---
+++ src/button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="g-button">按钮</button>
+</template>
+
+<script>
+export default { }
+</script>
+
+<style lang="scss" scoped>
+.g-button {
+  height: var(--button-height);
+  padding: 0 1em;
+  font-size: var(--font-size);
+  border: 1px solid var(--border-color);
+  border-radius: var(--border-radius);
+  background: var(--button-bg);
+  &:hover {
+    border-color: var(--border-button-hover);
+  }
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
+}
+
+</style>
