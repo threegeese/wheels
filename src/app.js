fc5262b726ecd35141ccbb2ff1827e972eb2c4eb
threegeese <19970913qjh@gmail.com>
2019-09-16 18:20:03 +0800

    use chai and chai-spies Unit Testing

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import ButtonGroup from './button-group.vue'
 import Button from './button.vue'
 
+import chai from 'chai'
+import spies from 'chai-spies'
+
+chai.use(spies)
+
 new Vue({
   el: '#app',
   components: {
@@ -9,3 +14,40 @@ new Vue({
     'g-button': Button
   }
 })
+
+// Unit Testing
+const expect = chai.expect
+{
+  const constructor = Vue.extend(Button)
+  const button = new constructor({
+    propsData: {
+      'icon': 'settings'
+    }
+  })
+  button.$mount()
+
+  let useElement = button.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#icon-settings')
+
+  button.$el.remove()
+  button.$destroy()
+}
+
+{
+  const constructor = Vue.extend(Button)
+  const gButton = new constructor({
+    propsData: {
+      'icon': 'settings'
+    }
+  })
+  gButton.$mount()
+
+  let spy = chai.spy(function () {})
+  gButton.$on('click', spy)
+
+  let button = gButton.$el
+  button.click()
+
+  expect(spy).to.have.been.called()
+}
