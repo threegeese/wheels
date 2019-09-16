771be89d490cdd18b390844f9883f14a9d70373e
threegeese <19970913qjh@gmail.com>
2019-09-16 12:49:01 +0800

    little change

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
+import ButtonGroup from './button-group.vue'
 import Button from './button.vue'
 
 new Vue({
   el: '#app',
   components: {
+    'g-button-group': ButtonGroup,
     'g-button': Button
   }
 })
---
+++ src/button-group.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="g-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import Button from './button.vue'
+export default {
+  components: {
+    'g-button': Button
+  }
+}
+</script>
+
+<style lang="scss">
+.g-button-group {
+  display: inline-flex;
+  > .g-button {
+    border-radius: 0;
+    &:not(:first-child) {
+      margin-left: -1px;
+    }
+    &:first-child {
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius);
+    }
+    &:last-child {
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+    }
+    &:hover {
+      z-index: 1;
+    }
+  }
+}
+</style>
